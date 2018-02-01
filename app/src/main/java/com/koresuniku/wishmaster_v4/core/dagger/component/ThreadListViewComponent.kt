adb874a8ab2128a8284729d4b8e690fba7f29d86
annie-koresuniku <koresuniku@fmail.com>
2018-02-01 07:17:30 +0200

    Tree refactoring

--- app/src/main/java/com/koresuniku/wishmaster_v4/core/dagger/component/ThreadListViewComponent.kt
+++ app/src/main/java/com/koresuniku/wishmaster_v4/core/dagger/component/ThreadListViewComponent.kt
@@ -2,6 +2,7 @@ package com.koresuniku.wishmaster_v4.core.dagger.component
 
 import com.koresuniku.wishmaster_v4.core.dagger.scope.ForDashboardView
 import com.koresuniku.wishmaster_v4.core.dagger.module.thread_list_scope.ThreadListViewModule
+import com.koresuniku.wishmaster_v4.core.dagger.scope.ForThreadListView
 import com.koresuniku.wishmaster_v4.core.thread_list.ThreadListPresenter
 import com.koresuniku.wishmaster_v4.ui.thread_list.ThreadListActivity
 import com.koresuniku.wishmaster_v4.ui.thread_list.ThreadListRecyclerViewAdapterI
@@ -11,7 +12,7 @@ import dagger.Component
  * Created by koresuniku on 01.01.18.
  */
 
-@ForDashboardView
+@ForThreadListView
 @Component(dependencies = [(ApplicationComponent::class)], modules = [(ThreadListViewModule::class)])
 interface ThreadListViewComponent {
 
